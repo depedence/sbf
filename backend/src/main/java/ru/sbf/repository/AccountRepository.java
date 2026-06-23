1aae4a12c97b1375c9d155f12cda6dafb8dede41
Alexander Averin <a.averin@rosintel.com>
2026-06-23 16:44:19 +0300

    add account logic (service+controller)

--- backend/src/main/java/ru/sbf/repository/AccountRepository.java
+++ backend/src/main/java/ru/sbf/repository/AccountRepository.java
@@ -4,9 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import ru.sbf.entity.Account;
 import ru.sbf.entity.User;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
-    Optional<Account> findByUser(User user);
+    List<Account> findAllByUser(User user);
+
+    Optional<Account> findByIdAndUser(Long id, User user);
 }
